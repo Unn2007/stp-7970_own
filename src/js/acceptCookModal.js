1a73ea285869c3f627c0f4051adbec953838142e
Anastasiia Bulba <fedinanastya@gmail.com>
2025-02-24 21:53:24 +0200

    modal cookies

---
+++ src/js/acceptCookModal.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const cookieModal = document.getElementById('cookieModal');
+  const acceptCookiesBtn = document.getElementById('acceptCookies');
+  const declineCookiesBtn = document.getElementById('declineCookies');
+
+  if (!localStorage.getItem('cookiesAccepted')) {
+    cookieModal.style.display = 'flex';
+  } else {
+    cookieModal.style.display = 'none';
+  }
+
+  acceptCookiesBtn.addEventListener('click', function () {
+    localStorage.setItem('cookiesAccepted', 'true');
+    cookieModal.style.display = 'none';
+  });
+
+  declineCookiesBtn.addEventListener('click', function () {
+    cookieModal.style.display = 'none';
+  });
+});
